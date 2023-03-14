9573b907922b58cb3c4f358ef9f967c2b4e76bb4
Santiago <santiago.gonzalezagudelo@gmail.com>
2023-03-14 13:58:25 -0500

    Terminar Página Final

--- JS/comprado.js
+++ JS/comprado.js
@@ -1,7 +1,8 @@
 let exportado=JSON.parse(localStorage.getItem('user'))
 let producto=document.getElementById('producto').innerHTML=`<h1>${exportado.producto}</h1>`
 
-console.log(exportado.producto)
+
+// console.log(exportado.producto)
 
 // producto.innerHTML=(`<h1>${exportado.producto}</h1>`)
 
